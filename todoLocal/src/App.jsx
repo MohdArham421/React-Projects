b6a6cd5102d9f6931e5986f29445d4767636aacf
Mohd Arham <mohdarham4210@email.com>
2026-02-19 16:40:01 +0530

    added functionailty in todoLocal

--- todoLocal/src/App.jsx
+++ todoLocal/src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {TodoProvider} from "./contexts"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
 
   return (
-    <>
+    <TodoProvider>
       <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
@@ -19,8 +18,8 @@ function App() {
                     </div>
                 </div>
             </div>
-    </>
+    </ TodoProvider>
   )
-}
+  }
 
 export default App
