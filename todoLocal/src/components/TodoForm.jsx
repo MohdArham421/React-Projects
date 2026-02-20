7280c284c093b80fad44c61e24c31256a772acb1
Mohd Arham <mohdarham4210@email.com>
2026-02-20 16:47:35 +0530

    Components added

--- todoLocal/src/components/TodoForm.jsx
+++ todoLocal/src/components/TodoForm.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import useTodo from "../contexts"
 
 function TodoForm() {
-  return (
-    <div>
-      
-    </div>
-  )
+  const [todo, setTodo] = useState("")
+  const {addTodo} = useTodo();
+
+  const add = (e) => {
+    e.preventDefault() //Page reload रोकता है.
+
+    if(!todo) return //Empty todo add करने से रोकता है.
+
+    addTodo({todo, completed: false}) //New todo object add करता है.
+    
+    setTodo("")    // Input field clear करता है.
+  }
+
+
+    return (
+        <form onSubmit={add} className="flex">
+            <input
+                type="text"
+                placeholder="Write Todo..."
+                className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+                value={todo}
+                onChange={(e) => setTodo(e.target.value)}
+           />
+            <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
+                Add
+            </button>
+        </form>
+    );
 }
 
-export default TodoForm
+export default TodoForm;
