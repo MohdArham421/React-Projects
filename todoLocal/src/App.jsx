ec2b9b48243e20836df5b0d1fed552fed6eda12c
Mohd Arham <mohdarham4210@email.com>
2026-02-20 22:39:00 +0530

    update

--- todoLocal/src/App.jsx
+++ todoLocal/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   }
 
   const toggleComplete = (id) => {
-    setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
   }
 
 
@@ -42,10 +42,18 @@ function App() {
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
-                        {/* Todo form goes here */} 
+                        {/* Todo form goes here */}
+                        <TodoForm /> 
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
+                        <div className='flex flex-wrap gap-y-3'>
+                          {todos.map((todo) => (
+                            <div key={todo.id} className='w-full'>
+                              <TodoItem todo={todo} />
+                            </div>
+                          ))}
+                        </div>
                     </div>
                 </div>
             </div>
